532d8da85a92176a93e42283b3bfe7fdc1938cc4
nakatani-takashi <nakatani_takashi@arsaga.jp>
2023-08-29 21:04:41 +0900

    とりあえず雑にgo_routerの導入

---
+++ lib/router.g.dart
@@ -0,0 +1,56 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'router.dart';
+
+// **************************************************************************
+// GoRouterGenerator
+// **************************************************************************
+
+List<RouteBase> get $appRoutes => [
+      $homeRoute,
+      $nextRoute,
+    ];
+
+RouteBase get $homeRoute => GoRouteData.$route(
+      path: '/home',
+      factory: $HomeRouteExtension._fromState,
+    );
+
+extension $HomeRouteExtension on HomeRoute {
+  static HomeRoute _fromState(GoRouterState state) => const HomeRoute();
+
+  String get location => GoRouteData.$location(
+        '/home',
+      );
+
+  void go(BuildContext context) => context.go(location);
+
+  Future<T?> push<T>(BuildContext context) => context.push<T>(location);
+
+  void pushReplacement(BuildContext context) =>
+      context.pushReplacement(location);
+
+  void replace(BuildContext context) => context.replace(location);
+}
+
+RouteBase get $nextRoute => GoRouteData.$route(
+      path: '/next',
+      factory: $NextRouteExtension._fromState,
+    );
+
+extension $NextRouteExtension on NextRoute {
+  static NextRoute _fromState(GoRouterState state) => const NextRoute();
+
+  String get location => GoRouteData.$location(
+        '/next',
+      );
+
+  void go(BuildContext context) => context.go(location);
+
+  Future<T?> push<T>(BuildContext context) => context.push<T>(location);
+
+  void pushReplacement(BuildContext context) =>
+      context.pushReplacement(location);
+
+  void replace(BuildContext context) => context.replace(location);
+}
