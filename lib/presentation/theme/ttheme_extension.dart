492be1ef02ef3b79c8624bc7e09efb844621feb2
nakatani-takashi <nakatani_takashi@arsaga.jp>
2023-09-02 09:20:38 +0900

    カスタムテーマの作成

---
+++ lib/presentation/theme/ttheme_extension.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+extension CustomTextStyles on TextTheme {
+  static const customText = TextStyle();
+}
+
+class CuctomColors {
+  static const Color customColor = Color(0xFFFBC812);
+}
