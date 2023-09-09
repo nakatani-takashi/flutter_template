89008bf71bbb7b145efd08288e946f71f1df0748
nakatani-takashi <nakatani_takashi@arsaga.jp>
2023-09-09 16:50:13 +0900

    fix: App()を分離したため、testのimportが変わってしまったので修正

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,10 +6,8 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
-
+import 'package:flutter_template/app.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:flutter_template/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
