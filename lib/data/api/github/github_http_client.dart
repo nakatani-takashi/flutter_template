bca6dd64742035ae62e9e0ca50d2ce2edc236138
nakatani-takashi <nakatani_takashi@arsaga.jp>
2023-09-12 10:23:30 +0900

    fix: フォルダ名のリネーム

--- lib/data/api/github/github_http_client.dart
+++ lib/data/api/github/github_http_client.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 import 'dart:io';
 
-import 'package:flutter_template/apis/exceptions.dart';
+import 'package:flutter_template/data/api/exceptions.dart';
 import 'package:http/http.dart' as http;
 
 /// GitHub API 用の HTTPクライアント
