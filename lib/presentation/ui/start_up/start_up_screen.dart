76629d82055cd1ad4bd4a73e3f9047985325ef8d
nakatani-takashi <nakatani_takashi@arsaga.jp>
2023-10-06 23:53:28 +0900

    update: StateUpScreen作成

---
+++ lib/presentation/ui/start_up/start_up_screen.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_template/domain/start_up/start_up_screen_entity.dart';
+import 'package:flutter_template/provider/core/loading_state_provider.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class StartUpScreen extends ConsumerWidget {
+  const StartUpScreen({
+    super.key,
+    required this.startUpScreenEntity,
+  });
+  final StartUpScreenEntity startUpScreenEntity;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      body: Center(
+        child: Stack(
+          children: [
+            Column(
+              children: [
+                Text(
+                  startUpScreenEntity.httpbinResponse.when(
+                    data: (data) => 'success',
+                    error: (error, _) => 'error',
+                    loading: () => 'loading',
+                  ),
+                ),
+                ElevatedButton(
+                  child: const Text('post action'),
+                  onPressed: () {
+                    startUpScreenEntity.postAction;
+                  },
+                ),
+                ElevatedButton(
+                  child: const Text('go main'),
+                  onPressed: () {
+                    startUpScreenEntity.goMain;
+                  },
+                ),
+              ],
+            ),
+            if (ref.watch(loadingStateProvider))
+              const CircularProgressIndicator(),
+          ],
+        ),
+      ),
+    );
+  }
+}
