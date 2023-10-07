496768cc93502c857ed717447f16098cd264d12a
nakatani-takashi <nakatani_takashi@arsaga.jp>
2023-10-07 22:11:14 +0900

    update: loadingStateUpdaterでログ出力できるように改修

--- lib/presentation/extension/loading_state_updater.dart
+++ lib/presentation/extension/loading_state_updater.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_template/provider/core/loading_state_provider.dart';
+import 'package:flutter_template/util/logger.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void loadingStateUpdater({
@@ -7,9 +8,19 @@ void loadingStateUpdater({
 }) {
   final notifier = widgetRef.watch(loadingStateProvider.notifier);
   for (final asyncValue in asyncValueList) {
-    if (asyncValue.isLoading) {
-      return notifier.show();
-    }
+    asyncValue.when(
+      data: (data) => {
+        logger.i('success: ${asyncValue.asData}: $data'),
+        notifier.hide(),
+      },
+      error: (error, _) => {
+        logger.w('error: ${asyncValue.asData}: $error'),
+        notifier.hide(),
+      },
+      loading: () => {
+        logger.i('loading: ${asyncValue.asData}'),
+        notifier.show(),
+      },
+    );
   }
-  return notifier.hide();
 }
