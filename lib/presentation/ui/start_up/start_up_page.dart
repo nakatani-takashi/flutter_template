4e9b16a01fe772f48b03ecbd044569520f644f3f
nakatani-takashi <nakatani_takashi@arsaga.jp>
2023-10-08 13:41:25 +0900

    update: provider層の更新

--- lib/presentation/ui/start_up/start_up_page.dart
+++ lib/presentation/ui/start_up/start_up_page.dart
@@ -3,8 +3,8 @@ import 'package:flutter_template/domain/ui/start_up/start_up_screen_entity.dart'
 import 'package:flutter_template/presentation/extension/loading_state_updater.dart';
 import 'package:flutter_template/presentation/navigation/main_route.dart';
 import 'package:flutter_template/presentation/ui/start_up/start_up_screen.dart';
-import 'package:flutter_template/provider/httpbin/get_httpbin_response.dart';
-import 'package:flutter_template/provider/httpbin/httpbin_usecase.dart';
+import 'package:flutter_template/provider/ui/httpbin/get_httpbin_response.dart';
+import 'package:flutter_template/provider/ui/httpbin/httpbin_usecase.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class StartUpPage extends ConsumerWidget {
